a881ca060d06878c96230786158944a86c9d012d
D G <gornijinjener@gmail.com>
2024-05-16 00:21:09 +0300

    model

--- introduction/schedule/admin.py
+++ introduction/schedule/admin.py
@@ -2,24 +2,15 @@ from django.contrib import admin
 from .models import Group, Soloists, Performances, Schedules
 
 
+from django.contrib import admin
+from .models import Schedules
+
 class SchedulesAdmin(admin.ModelAdmin):
-    list_display = ('number', 'performance_title', 'solist_names', 'group_names',  'is_published')
+    list_display = ('number', 'performance_title', 'solist_names', 'group_names', 'is_published')
     list_filter = ('is_published', 'performance', 'group', 'solist')
     search_fields = ['performance__title', 'solist__name', 'group__name']
     ordering = ['number']
-
-    def performance_title(self, obj):
-        return obj.performance.title
-    performance_title.short_description = 'Выступление'
-
-    def solist_names(self, obj):
-        return ', '.join([s.name for s in obj.solist.all()])
-    solist_names.short_description = 'Солисты'
-
-    def group_names(self, obj):
-        return ', '.join([g.name for g in obj.group.all()])
-    group_names.short_description = 'Группы'
-
+    change_form_template = 'admin/schedules/change_form.html'
 
 admin.site.register(Schedules, SchedulesAdmin)
 
