c4905b2d0b372accdab740a212acd52abb742284
D G <gornijinjener@gmail.com>
2024-05-15 23:54:04 +0300

    model

--- introduction/schedule/admin.py
+++ introduction/schedule/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Group, Soloists, Performances, Schedules
 
+
+class SchedulesAdmin(admin.ModelAdmin):
+    list_display = ('number', 'performance', 'solist', 'group', 'group_2', 'is_published')
+    list_filter = ('is_published', 'performance', 'group', 'solist')
+    search_fields = ['performance__title', 'solist__name', 'group__name']
+    ordering = ['number']
+
+
 admin.site.register(Group)
 admin.site.register(Soloists)
 admin.site.register(Performances)
-admin.site.register(Schedules)
+admin.site.register(Schedules, SchedulesAdmin)
--- introduction/schedule/models.py
+++ introduction/schedule/models.py
@@ -1,55 +1,56 @@
 from django.db import models
-from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
 class Group(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название группы')  # Название группы
-    
-    class Meta:
-        verbose_name =  _("Группа")  # название на русском для одной группы
-        verbose_name_plural = _("Группы")  # название на русском для нескольких групп
+    name = models.CharField(max_length=255, verbose_name=_('Название группы'))
 
     def __str__(self):
-        return self.title
-    
-    
+        return self.name
+
+    class Meta:
+        verbose_name = _('Группа')
+        verbose_name_plural = _('Группы')
+        ordering = ['name']
+
+
 class Soloists(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Имя солиста')  # Имя солиста
-    
-    class Meta:
-        verbose_name =  _("Солист")  # название на русском для одного солиста
-        verbose_name_plural = _("Солисты")  # название на русском для нескольких солистов
+    name = models.CharField(max_length=255, verbose_name=_('Имя солиста'))
 
     def __str__(self):
-        return self.title
+        return self.name
+
+    class Meta:
+        verbose_name = _('Солист')
+        verbose_name_plural = _('Солисты')
+        ordering = ['name']
 
 
 class Performances(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Наименование номера')
-    
-    class Meta:
-        verbose_name =  _("Номер")
-        verbose_name_plural = _("Номера")
+    title = models.CharField(max_length=255, verbose_name=_('Название выступления'))
 
     def __str__(self):
         return self.title
-    
+
+    class Meta:
+        verbose_name = _('Выступление')
+        verbose_name_plural = _('Выступления')
+        ordering = ['title']
+
 
 class Schedules(models.Model):
     ORDER_CHOICES = [(i, str(i)) for i in range(1, 100)]  # Предопределенные значения для номера по порядку
 
-    number = models.IntegerField(choices=ORDER_CHOICES, verbose_name='Номер по порядку')
-    performance = models.ForeignKey(Performances, on_delete=models.CASCADE, verbose_name='Выступление')
-    solist = models.ForeignKey(Soloists, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Солист')
-    solist_2 = models.ForeignKey(Soloists, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Солист 2')
-    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Группа')
-    group_2 = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Группа 2')
-    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
+    number = models.IntegerField(choices=ORDER_CHOICES, verbose_name=_('Номер по порядку'))
+    performance = models.ForeignKey(Performances, on_delete=models.CASCADE, verbose_name=_('Выступление'))
+    solist = models.ManyToManyField(Soloists, related_name='schedules', verbose_name=_('Солисты'))
+    group = models.ManyToManyField(Group, related_name='schedules', verbose_name=_('Группы'))
+    group_2 = models.ManyToManyField(Group, related_name='schedules_2', verbose_name=_('Группы 2'))
+    is_published = models.BooleanField(default=False, verbose_name=_('Опубликовано'))
 
     def __str__(self):
         return f'{self.number} - {self.performance.title}'
 
     class Meta:
-        verbose_name = 'Расписание'
-        verbose_name_plural = 'Расписания'
+        verbose_name = _('Расписание')
+        verbose_name_plural = _('Расписания')
         ordering = ['number']
