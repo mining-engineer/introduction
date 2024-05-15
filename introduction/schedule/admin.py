3425b0373d1b0ac30a0eda7245f08e1b47660b64
D G <gornijinjener@gmail.com>
2024-05-15 23:20:07 +0300

    model

--- introduction/schedule/admin.py
+++ introduction/schedule/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Group
+from .models import Group, Soloists, Performances
 
 admin.site.register(Group)
+admin.site.register(Soloists)
+admin.site.register(Performances)
--- introduction/schedule/models.py
+++ introduction/schedule/models.py
@@ -1,13 +1,35 @@
 from django.db import models
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 class Group(models.Model):
-    title = models.CharField(max_length=255)  # Название группы
+    title = models.CharField(max_length=255, verbose_name='Название группы')  # Название группы
     
     class Meta:
-        verbose_name = "Группа"  # название на русском для одной группы
-        verbose_name_plural = "Группы"  # название на русском для нескольких групп
+        verbose_name =  _("Группа")  # название на русском для одной группы
+        verbose_name_plural = _("Группы")  # название на русском для нескольких групп
+
+    def __str__(self):
+        return self.title
+    
+    
+class Soloists(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Имя солиста')  # Имя солиста
+    
+    class Meta:
+        verbose_name =  _("Солист")  # название на русском для одного солиста
+        verbose_name_plural = _("Солисты")  # название на русском для нескольких солистов
 
     def __str__(self):
         return self.title
 
+
+class Performances(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Наименование номера')
+    
+    class Meta:
+        verbose_name =  _("Номер")
+        verbose_name_plural = _("Номера")
+
+    def __str__(self):
+        return self.title
