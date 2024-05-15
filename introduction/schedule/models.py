9c525611f2b8257978cba3325bf12c0767a9554e
D G <gornijinjener@gmail.com>
2024-05-15 23:02:32 +0300

    model

--- introduction/schedule/models.py
+++ introduction/schedule/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Group(models.Model):
+    title = models.CharField(max_length=255)  # Название группы
+    
+    class Meta:
+        verbose_name = "Группа"  # название на русском для одной группы
+        verbose_name_plural = "Группы"  # название на русском для нескольких групп
+
+    def __str__(self):
+        return self.title
+
